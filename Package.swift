a533eff271b7db72b4c5a6ecfdfe86706c5b61a7
Mihael Isaev <isaev.mihael@gmail.com>
2020-03-02 19:44:07 +0400

    Package.swift: bump to swift 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,14 +14,21 @@ let package = Package(
         dependencies: [
         .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from:"1.0.0"),
         .package(url: "https://github.com/MihaelIsaev/SwifQLNIO.git", from:"2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.10.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-beta.2"),
-        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-rc"),
         ],
     targets: [
-        .target(name: "SwifQLVapor", dependencies: ["NIO", "SwifQL", "SwifQLNIO", "Fluent", "FluentPostgresDriver", "Vapor"]),
-        .testTarget(name: "SwifQLVaporTests", dependencies: ["SwifQLVapor"]),
-        ]
+        .target(name: "SwifQLVapor", dependencies: [
+            .product(name: "SwifQL", package: "SwifQL"),
+            .product(name: "SwifQLNIO", package: "SwifQLNIO"),
+            .product(name: "Fluent", package: "fluent"),
+            .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+            .product(name: "Vapor", package: "vapor"),
+        ]),
+        .testTarget(name: "SwifQLVaporTests", dependencies: [
+            .target(name: "SwifQLVapor")
+        ]),
+    ]
 )
