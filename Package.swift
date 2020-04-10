b7271558bf85a3816c76aeb0707b2493190c9190
Mihael Isaev <isaev.mihael@gmail.com>
2020-04-10 13:08:58 +0400

    Bump to Vapor 4 release

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         dependencies: [
         .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from:"1.0.0"),
         .package(url: "https://github.com/MihaelIsaev/SwifQLNIO.git", from:"2.0.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-rc"),
