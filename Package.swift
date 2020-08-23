77099ccbebbbfc2429f46070e806a776041c897d
Maxim Anisimov <maxim@invoodoo.com>
2020-08-23 22:32:19 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
         .package(url: "https://github.com/MihaelIsaev/SwifQL.git", from:"1.0.0"),
         .package(url: "https://github.com/MihaelIsaev/SwifQLNIO.git", from:"2.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0-rc"),
-        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0-rc"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
         ],
     targets: [
         .target(name: "SwifQLVapor", dependencies: [
